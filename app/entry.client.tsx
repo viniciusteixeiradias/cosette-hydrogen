a47eb86caef29845bec7622f83255f4b36eaadc1
viniciusteixeiradias <viniciustdias2001@gmail.com>
2023-07-23 17:53:03 +1000

    fix: fix bug hydration mismatch

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,12 +1,4 @@
-import {RemixBrowser} from '@remix-run/react';
-import {startTransition, StrictMode} from 'react';
-import {hydrateRoot} from 'react-dom/client';
+import { RemixBrowser } from '@remix-run/react'
+import { hydrate } from 'react-dom'
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>,
-  );
-});
+hydrate(<RemixBrowser />, document)
